25f88fc603d8230b32a518e8dc31fb8be359b38f
amir mekkaoui <amirmekk@outlook.com>
2020-11-24 10:41:53 +0100

    links to assets and social media accounts

---
+++ lib/config/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  static const avatar = 'assets/avatar.jpg';
+  static const twitter = 'assets/twitter.png';
+  static const github = 'assets/github.png';
+  static const linkedin = 'assets/linkedin.png';
+  static const youtube = 'assets/youtube.png';
+  static const instagram = 'assets/instagram.png';
+  static const stackoverflow = 'assets/stackoverflow.png';
+  static const tarikh19 = 'assets/tarikh19.jpg';
+  static const puzzle_store = 'assets/puzzle_store.jpg';
+  static const bibliolater = 'assets/bibliolater.jpg';
+}
---
+++ lib/config/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const twitter_url = 'https://twitter.com/mekk_amir';
+  static const github_url = 'https://github.com/amirmekk';
+  static const stackoverflow_url =
+      'https://stackoverflow.com/users/8461236/amir-mekkaoui';
+  static const youtube_url =
+      'https://www.youtube.com/channel/UCmJCEnxSFbTzoN6SYCQdIXw?view_as=subscriber';
+  static const linkedin_url =
+      'https://www.linkedin.com/in/amir-m-21947a102'; // TODO: verify it's working
+  static const instagram_url = 'https://www.instagram.com/mekkamir/';
+}
